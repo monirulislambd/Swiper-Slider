9c816352bb47e84eb9588a90799cba52e79e20a2
Monirul Islam <monirulislam.nbr@gmail.com>
2024-12-22 10:37:45 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    const mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        effect: 'cube',
+        grabCursor: true,
+
+        autoplay:{
+            delay: 3000,
+            disableOnInteration: false,
+        },
+        speed: 1000,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            type: 'fraction',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+          },
+      });
+});
